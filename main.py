d9ae3af4f63f5b31e67754d35ac6bb2366b67545
Riccardo Pasianotto <riccardo.pasianotto@gmail.com>
2022-06-15 16:48:35 +0200

    Init project

---
+++ main.py
@@ -0,0 +1,9 @@
+# Air Labels
+# A tool to support labeling operations of machine learning projects.
+
+def hello_airlabels(name: str):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    hello_airlabels('AirLabels')
